e7f9ed63bbbaed8dbfba69b1a19602fd1de8c265
jeremypmobley <jeremypmobley@gmail.com>
2017-08-30 19:12:09 -0500

    Adding send Brew and View text code

---
+++ send_brew_view_text/send_brew_view_texts.bat
@@ -0,0 +1,3 @@
+cd C:\Users\Jeremy\Anaconda2
+python C:\Users\Jeremy\Documents\GitHub\nonsense\send_brew_view_text\send_brew_view_texts.py
+
---
+++ send_brew_view_text/send_brew_view_texts.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+############################################################################
+
+import pandas as pd
+import twilio
+from twilio.rest import Client
+from bs4 import BeautifulSoup
+import urllib
+import time
+
+# import Twilio Credentials
+from twilio_credentialsguy import *
+
+
+def remove_double_spaces(string):
+    """
+    Function to remove double spaces from a string
+    """
+    return ' '.join(string.split())
+
+
+def get_brew_view_movies():
+    """
+    Function to scrape brew and view website
+    Returns list of first two movies in first table of website
+    """
+    website = "http://brewview.com/"
+    r = urllib.urlopen(website).read()
+    soup = BeautifulSoup(r, "lxml")
+    html_table = pd.read_html(soup.select("table")[1].prettify())[0]
+    return [html_table[1][0], html_table[1][1]]
+
+
+def main():
+	brew_view_movies = get_brew_view_movies()
+	for i in range(len(brew_view_movies)):
+		message_body = "Brew & View is playing " + remove_double_spaces(brew_view_movies[i])
+		client = Client(ACCOUNT_SID, AUTH_TOKEN)
+		client.messages.create(
+			to="+17045768532", 
+			from_="+13123132044", 
+			body=message_body
+		)
+		time.sleep(10)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
